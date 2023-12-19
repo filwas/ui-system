688661f0547a0039e9067842f7e576c9d150b96d
Filip Wąs <69075808+filwas@users.noreply.github.com>
2023-12-19 17:59:30 +0100

    Initial commit

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  collectCoverage: true,
+  collectCoverageFrom: ["src/**/*.{js,jsx}"],
+  coverageDirectory: "coverage",
+  testEnvironment: "jsdom",
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
+  moduleNameMapper: {
+    "\\.(css|less|scss|sass)$": "identity-obj-proxy",
+    "\\.(gif|ttf|eot|svg)$": "<rootDir>/__mocks__/fileMock.js",
+  },
+};
